9fb79525d085be00c3fa8e3c78f0498bc9cff092
Hasan ÖKTEN <hokten@gmail.com>
2018-04-26 12:02:28 +0300

    Web sayfasının kök URLsinden sorumlu Controller UrunController olarak değiştirildi. Bu controllerın arsayılan action metodu Listele yapıldı.

--- SporDukkani.WebUI/App_Start/RouteConfig.cs
+++ SporDukkani.WebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SporDukkani.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Urun", action = "Listele", id = UrlParameter.Optional }
             );
         }
     }
