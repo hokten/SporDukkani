ffe23631068403d1a59fdce74d402c7d898ca364
Hasan ÖKTEN <hokten@gmail.com>
2018-04-26 11:40:43 +0300

    Ürün listelemek için kullanacağımızx UrunController.cs controllerı eklendi. Dependency injection oluşturmak için IUrunlerFabrikasi türünde fabrika özelliği oluşturuldu.

---
+++ SporDukkani.WebUI/Controllers/UrunController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SporDukkani.Veriler.Soyut;
+using SporDukkani.Veriler.Varliklar;
+
+namespace SporDukkani.WebUI.Controllers
+{
+    public class UrunController : Controller
+    {
+        private IUrunlerFabrikasi fabrika;
+        public UrunController(IUrunlerFabrikasi fbr)
+        {
+            this.fabrika = fbr;
+        }
+
+        // GET: Urun
+        public ActionResult Listele()
+        {
+            return View(fabrika.Urunler);
+        }
+    }
+}
