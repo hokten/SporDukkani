fa0b2c05ac5e31b907f93de3546b0001624a2e53
Hasan ÖKTEN <hokten@gmail.com>
2018-04-26 11:09:46 +0300

    Modelleri paketlemek için SporDukkani.Veriler projesine Varlıklar klasörü eklendi. Varliklar isim uzayı altında Urun.cs dosyası ile Urun modeli oluşturuldu.

---
+++ SporDukkani.Veriler/Varliklar/Urun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SporDukkani.Veriler.Varliklar
+{
+    class Urun
+    {
+        public int UrunID { get; set; }
+        public string Isim { get; set; }
+        public string Aciklama { get; set; }
+        public decimal Fiyat { get; set; }
+        public string Kategori { get; set; }
+    }
+}
