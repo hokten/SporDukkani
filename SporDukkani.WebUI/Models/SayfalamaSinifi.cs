07194ae1ac84d9f55d7cfdd6dd998936358fdeb7
Hasan ÖKTEN <hokten@gmail.com>
2018-05-04 09:38:02 +0300

    Sayfalama işlemini gerçekleştirecek olan SayfalamSinifi eklendi.

---
+++ SporDukkani.WebUI/Models/SayfalamaSinifi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SporDukkani.WebUI.Models
+{
+    public class SayfalamaSinifi
+    {
+        public int ToplamKayitSayisi { get; set; }
+        public int SayfaBasinaDusenKayitSayisi { get; set; }
+        public int BulunulanSayfa { get; set; }
+        public int ToplamSayfaSayisi
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)ToplamKayitSayisi / SayfaBasinaDusenKayitSayisi);
+            }
+        }
+    }
+}
